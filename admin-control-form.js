f75e22b1fe04e9f69761a8134d5fd1722e811be5
prateekpm123 <prateekpm123@gmail.com>
2020-03-14 23:18:50 +0530

    Adding some dynamic content to the admin form

--- admin-control-form.js
+++ admin-control-form.js
@@ -1,43 +1,81 @@
-function appear() {
-    $('.pop').show();
-}
-
-function goback() {
-    $('.pop').hide();
-}
-
-function area_increase() {
-
-    $('#area-increase').keypress(function () 
+$(document).ready(
+    function()
     {
-        var keyvalue = this.value;
-        // alert('in keypress function');
-        return(keyvalue);    
-        if (key == 13) {
-            alert('working');
+        $('#create').click(function ()
+        {
+            $('#makingArea').append('<textarea name="question" placeholder="Enter the questions..."  onmouseover="" onkeypress="area_increase()" cols="30" rows="10" class="questionarea">');
+
+        });
+
+        // $('#delete').on('click','.questionarea', function() 
+        // {
+        //     $(this).remove('.questionarea');
+        // });
+        // function appear() {
+        //     $('.pop').show();
+        // }
+
+        // function goback() {
+        //     $('.pop').hide();
+        // } 
+
+        // function area_increase() {
+
+        //     $('#area-increase').keypress(function () {
+        //         var keyvalue = this.value;
+        //         // alert('in keypress function');
+        //         return (keyvalue);
+        //         if (key == 13) {
+        //             alert('working');
 
 
-            var c = document.getElementById('area-increase');
-            // Q1. Why is it different from above one. This is 324 while earlier it was 320. See the output.
-            // console.log(c.clientWidth);
+        //             var c = document.getElementById('area-increase');
+        //             // Q1. Why is it different from above one. This is 324 while earlier it was 320. See the output.
+        //             // console.log(c.clientWidth);
 
-            // Q2. How to update the code to increase the width by 10rem.
-            $c.css({ width: $c.width() + 10 }); //It adds 10 pixels here.
+        //             // Q2. How to update the code to increase the width by 10rem.
+        //             $c.css({ width: $c.width() + 10 }); //It adds 10 pixels here.
 
-        }
+        //         }
+        //     }
+
+        //     );
+
+        //     function test() {
+        //         alert('in mouse over');
+
+        //         var c = document.getElementById('area-increase');
+        //         // Q1. Why is it different from above one. This is 324 while earlier it was 320. See the output.
+        //         // console.log(c.clientWidth);
+
+        //         // Q2. How to update the code to increase the width by 10rem.
+        //         $c.css({ width: $c.width() + 10 }); //It adds 10 pixels here.
+        //     }
+
+        // }
+
+        
+
     }
-    
-    );
+);
 
-function test() {
-    alert('in mouse over');
+// function deleting() {
+//     $('#delete').on('click', '.questionarea',
+//         $(this).remove('.questionarea'));
+//     );
+// }
 
-    var c = document.getElementById('area-increase');
-    // Q1. Why is it different from above one. This is 324 while earlier it was 320. See the output.
-    // console.log(c.clientWidth);
 
-    // Q2. How to update the code to increase the width by 10rem.
-    $c.css({ width: $c.width() + 10 }); //It adds 10 pixels here.
-}
-    
-}
+
+// function toremove() 
+// {
+    // $('#makingArea').on('click', '.questionarea', function () {
+        // $(this).remove();
+    // });
+// }
+
+$('#delete').click(function()
+{
+    // $('body').css('background-color','black');
+    $('.questionarea').remove();
+});
