a7426d187ca3e7a0f8210876778e73d56608a523
prateekpm123 <prateekpm123@gmail.com>
2020-03-28 17:25:36 +0530

    Very minor changes.

--- teacherform.php
+++ teacherform.php
@@ -102,7 +102,33 @@ if (isset($_POST['submit']))
 
     
 
-<div class="container">
+<!-- <div class="container-fluid"> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-header">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="forms.php">Forms </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+                <li class="nav-item  right">
+                    <a class="nav-link" href="web/index.php">Admin panel</a>
+                </li>
+                <!-- <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li> -->
+            </ul>
+        </div>
+    </nav>
     <div class="row col-md-6 offset-md-3" id="contentToPrint" >
         
             <h2></h2>
@@ -341,26 +367,6 @@ if (isset($_POST['submit']))
                     </div>
                     <div class="col-md-6 offset-md-3">      
                     <!-- <button class="btn btn-primary" type="submit" name="submit">Submit</button> -->
-<<<<<<< HEAD
-                    <input type="submit" class="btn btn-primary" name="submit" value="Submit">  
-                    <br>
-                    <button class='btn btn-primary' onclick="print()">Print</button>  
-                </form>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-2 col-sm-1"></div>
-
-    </div>
-    <script type="text/javascript">    
-        function print() {
-            window.print();
-        }
-    </script>
-</body>
-
-
-
-=======
                     <input type="submit" class="btn btn-primary" name="submit" value="Submit" id="first" > 
                     <button onclick="myfun()" class="btn btn-primary" id= "second" >Print</button> 
                     </div>
@@ -371,7 +377,6 @@ if (isset($_POST['submit']))
         </div>       
             
         
->>>>>>> eded93fed0bed9d0694efe56920823debce6a1ad
 
     
     
