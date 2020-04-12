aa0bd7add863bbb847d400fcd56280b686c03c48
aniket0408 <singh.aniket0408@gmail.com>
2020-04-12 10:34:14 +0530

    question inserted in database

--- submit.php
+++ submit.php
@@ -1,8 +1,29 @@
 <?php 
+
+$con = mysqli_connect('localhost','root','');
+// mysqli_select_db($con, 'final_year_project');
+mysqli_select_db($con, 'test');
+
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+
+
 $question = $_POST['question'];
-$answer = $_POST['answer'];
-echo $question;
-echo '</br>';
-echo $answer;
+// $answer = $_POST['answer'];
+
+$add = "insert into questions(Questions) values ('$question')";
+// mysqli_query($con, $add);
+if (mysqli_query($con, $add)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $add . "<br>" . mysqli_error($con);
+}
+// echo $question;
+// echo '</br>';
+// echo $answer;
+// echo "inserted successfully";
 
 ?> 
