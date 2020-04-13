71a0122fd1b3182c858a524494ad31f2ec184964
prateekpm123 <prateekpm123@gmail.com>
2020-04-14 00:22:09 +0530

    New database Added from aniket to my local system

--- submit.php
+++ submit.php
@@ -1,19 +1,6 @@
 <?php 
 
 
-
-
-// $con = mysqli_connect('localhost','root','');
-// // mysqli_select_db($con, 'final_year_project');
-// mysqli_select_db($con, 'test');
-
-// if (!$con) {
-//   die("Connection failed: " . mysqli_connect_error());
-// }
-
-// echo "Connected successfully";
-
-
 $question = $_POST['question'];
 $answer = $_POST['answer'];
 // $id = $_POST['id'];
@@ -32,22 +19,37 @@ for( $i=0; $i < count($answer); $i++ )
 echo '</br>';
 
 
-// $add = "insert into questions(Questions) values ('$question')";
-// // mysqli_query($con, $add);
-// if (mysqli_query($con, $add)) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $add . "<br>" . mysqli_error($con);
-// }
+$con = mysqli_connect('localhost','root','');
+// mysqli_select_db($con, 'final_year_project');
+// mysqli_select_db($con, 'test');
+
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
 
 
 
-// echo "inserted successfully";
 
-// $ans = "insert into answers(Answers) values ('$answer')";
-// if (mysqli_query($con, $ans)) {
-//   echo "Answer record created successfully";
-// } else {
-//   echo "Error: " . $ans . "<br>" . mysqli_error($con);
-// }
-// ?> 
+
+$add = "insert into questions(Questions) values ('$question')";
+// mysqli_query($con, $add);
+if (mysqli_query($con, $add)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $add . "<br>" . mysqli_error($con);
+}
+
+
+
+echo "inserted successfully";
+
+$ans = "insert into answers(Answers) values ('$answer')";
+if (mysqli_query($con, $ans)) {
+  echo "Answer record created successfully";
+} else {
+  echo "Error: " . $ans . "<br>" . mysqli_error($con);
+}
+
+?> 
