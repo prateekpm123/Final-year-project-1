f1ab0e6e10ff80e65d359ee9ed50e77ed196360c
prateekpm123 <prateekpm123@gmail.com>
2020-04-18 13:20:56 +0530

    Sorted, The new creation part, now the only part is to sort the multiple choice option saving to the database and then displaying from the database

--- admin-control-form.js
+++ admin-control-form.js
@@ -205,7 +205,208 @@ function addMultipleChoice(info) {
   multipleChoiceCounter++;
 }
 
+
 function create(btnclass, btnid) {
+  // let saveId = "sub"+questionAreaCounter;
+  // ajaxToSave(saveId, btnid);
+
+  // ######################  All the common tags used for dynamically creating  ############################
+
+  // This creates the Questionset Div tag with gets appended to form-page1
+  let questionsetDivTag =
+    '<div class="question-set" id="questionset' +
+    questionAreaCounter +
+    '"></div>';
+
+  let optionsetDivTag =
+    '<div class="options-set" id="optionset' + questionAreaCounter + '"></div>';
+
+  let questionAreaCounter2 = questionAreaCounter;
+  questionAreaCounter2++;
+  // This creates the textareatag for the question with gets appended to the questionset Div tag
+  let questionTextareaTag =
+    '<textarea name="question' +
+    questionAreaCounter +
+    '" placeholder="Enter the questions..."  style="overflow:hidden"  onkeyup="AutoGrowTextArea(this)" cols="30" rows="3" id="Q' +
+    questionAreaCounter +
+    '" class="questionarea questionsetComponents' +
+    questionAreaCounter2 +
+    '"></textarea>';
+
+  // *************** From here and down, we'll be creating a structure for other options to land *************
+
+  // This is created inside the id = optionset
+  let optionsetRowDivTag =
+    '<div class="row option-setrow" id="optionsetrow' +
+    questionAreaCounter +
+    '"></div>';
+
+  // There will be 5 columns inside this row tag, and this is the first one
+  let optionset1stColDivTag =
+    '<div class="col-lg-2 col-md-2 col-sm-2 col-xm-2" id="optionCol-1-' +
+    questionAreaCounter +
+    '"></div>';
+  // This goes under the optionset1stColTag
+  let saveButtonTag =
+    '<button id="' +
+    questionAreaCounter +
+    '" class="btn btn-primary savebtn" onclick="ajaxToSave(this.id,' +
+    btnid +
+    ')"> Save</button>';
+
+  // This is the 2nd column
+  let optionset2ndColDivTag =
+    '<div class="col-lg-2 col-md-2 col-sm-2 col-xm-2" id="optionCol-2-' +
+    questionAreaCounter +
+    '"></div>';
+  // The results of the MySQL query sucess rate are stored in here
+  let resultShowingspanTag = '<span id="result0"></span>';
+
+  // This is the 3rd column
+  let optionset3rdColDivTag =
+    '<div class="col-lg-5 col-md-5 col-sm-5 col-xm-5" id="optionCol-3-' +
+    questionAreaCounter +
+    '"></div>';
+  // This is the button which will appear when multiple choice options is clicked
+  let addingMultipleChoiceBtnsButtonTag =
+    '<button class="btn btn-primary options' +
+    questionAreaCounter +
+    '" onclick="createMultipleChoice(' +
+    questionAreaCounter +
+    ')" id="createMultipleChoice' +
+    questionAreaCounter +
+    '">Add Multiple choice</button>';
+
+  // This the 4th Column
+  let optionset4thColDivTag =
+    '<div class="col-lg-1 col-md-1 col-sm-1 col-xm-1" id="optionCol-4-' +
+    questionAreaCounter +
+    '"></div>';
+  // This is a DropDownButton From Bootstrap which is used for choosing the options for the question, like long Answer, Multiple choice
+  let dropDownButton =
+    '<div class="input-group-prepend" id="optionbtns' +
+    questionAreaCounter +
+    '"> <button class="btn btn-outline-secondary dropdown-toggle optionbtn-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">+</button> <div class="dropdown-menu"> <button class="dropdown-item options' +
+    questionAreaCounter +
+    '" onclick="createMultipleChoice(this.className)">Multiple choice</button> <button class="dropdown-item options' +
+    questionAreaCounter +
+    '" href="#">Ratings</button> <button class="dropdown-item options' +
+    questionAreaCounter +
+    '" onclick="createLongAnswer(this.className)">Long Answer</button> <button class="dropdown-item options' +
+    questionAreaCounter +
+    '">Short Answer</button> </div> </div > ';
+
+  // This is the 5th Column
+  let optionset5thDivCol =
+    '<div class="col-lg-1 col-md-1 col-sm-1 col-xm-1" id="optionCol-5-' +
+    questionAreaCounter +
+    '"></div>';
+  // This is also a dropDown button from Bootstrap but its use it to have extra options like delete and etc
+  let dropDownButton2 =
+    '<div class="input-group-prepend"> <button class="btn btn-outline-secondary dropdown-toggle optionbtn-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Del</button> <div class="dropdown-menu"> <button class="dropdown-item extraOptions' +
+    questionAreaCounter +
+    '">Delete Choice</button> <button class="dropdown-item extraOptions' +
+    questionAreaCounter +
+    '">Another action</button> <button class="dropdown-item extraOptions' +
+    questionAreaCounter +
+    '">Something else here</button> <div role="separator" class="dropdown-divider"></div> <button class="dropdown-item extraOptions' +
+    questionAreaCounter +
+    '">Separated link</button> </div> </div>';
+
+  // This is to rest the counter for the multiple choice inputs spaces and for creating their UNIQUE ID's
+  multipleChoiceCounter = 1;
+
+  // Question set div tag generation
+  // Here both the questions and there options like longanswer's textareatag and multiplechoice's input tag goes
+  $("#form-page1").append(questionsetDivTag);
+
+  // This tells us, there are more components then just question area
+
+  // Options set Div tag Generations
+  $("#form-page1").append(optionsetDivTag);
+
+  let questionsetno = "#questionset" + questionAreaCounter;
+
+  let optionset = "#optionset" + questionAreaCounter;
+
+  let rowOptionset = "#optionsetrow" + questionAreaCounter;
+  let col1Optionset = "#optionCol-1-" + questionAreaCounter;
+  let col2Optionset = "#optionCol-2-" + questionAreaCounter;
+  let col3Optionset = "#optionCol-3-" + questionAreaCounter;
+  let col4Optionset = "#optionCol-4-" + questionAreaCounter;
+  let col5Optionset = "#optionCol-5-" + questionAreaCounter;
+
+  // This is will create a div tag which will get appended to form-page1 div tag, and it will contain a textarea for writing the Questions and it options like Long answer and Multiple choice questions
+  $(questionsetno).append(questionTextareaTag);
+
+  // This is will create a div tag which will get appended to form-page1 div tag, and it will be for options like save but and convert button etc
+  $(optionset).append(optionsetRowDivTag);
+
+  // This is the 1st column and the contents are just besides it
+  $(rowOptionset).append(optionset1stColDivTag);
+  $(col1Optionset).append(saveButtonTag);
+
+  // This is the 2nd column and the contents are just besides it
+  $(rowOptionset).append(optionset2ndColDivTag);
+  $(col2Optionset).append(resultShowingspanTag);
+
+  // This is the 3rd column and the contents are just besides it
+  $(rowOptionset).append(optionset3rdColDivTag);
+  $(col3Optionset).append(addingMultipleChoiceBtnsButtonTag);
+
+  let createMultipleChoice = "#createMultipleChoice" + questionAreaCounter;
+  $(createMultipleChoice).hide();
+
+  // This is the 4th column and the contents are just besides it
+  $(rowOptionset)
+    .append
+  // optionset4thColDivTag
+  ();
+  // *************** Code for bootstrap dropdownbutton *****************
+  $(col4Optionset)
+    .append
+  // dropDownButton
+  ();
+
+  // This is the 5th column and the contents are just besides it
+  $(rowOptionset).append(optionset5thDivCol);
+  $(col5Optionset).append(dropDownButton2);
+
+  // createLongAnswer(Class, id);
+  // **************** My made option buttons *****************
+
+  // $('#form-page1').append(
+  //     // '< button class="btn btn-primary textarea-btn" name="submit" id="optionsappear" > + < /button>'
+  //     '<button class="btn btn-primary textarea-btn" name="submit"  id="optionsappear' + questionAreaCounter + '" onclick=showOptions(this.id)>+</button>'
+  // );
+  // $('#form-page1').append(
+  //     '<div id="optionsarea' + questionAreaCounter + '" class="optionsareaclass"></div>'
+  // );
+  // let questionCounter = "#optionsarea"+questionAreaCounter;
+  // $(questionCounter).append(
+  //     '<button class="options' + questionAreaCounter + ' optionbtns"> Options</button>                <button class= "options' + questionAreaCounter + ' optionbtns"> Ratings</button>            <button class="options' + questionAreaCounter + ' optionbtns">Answers</button>'
+  // );
+
+  // console.log("btnid ", btnid);
+  // alert('before multiple choice');
+
+  if (btnid == 1) {
+    addMultipleChoice(questionAreaCounter);
+    // alert('in if else condition of multiple choice');
+  } else if (btnid == 2) {} else if (btnid == 3) {
+    // createLongAnswer(questionAreaCounter);
+    // alert('in if else condition of long answer');
+  } else if (btnid == 4) {}
+
+  questionAreaCounter++;
+
+  // div1.appendChild(textarea);
+  // provideId(questionAreaCounter);
+  // $("#tbl2").append('<input type="checkbox" id="' + firstId + '-' + secondId + '" >');
+}
+
+
+function createBefore(btnclass, btnid) {
   // let saveId = "sub"+questionAreaCounter;
   // ajaxToSave(saveId, btnid);
   console.log('QuestionAreaCounter is ',questionAreaCounter);
@@ -452,14 +653,14 @@ function creatingTheExistingContent(questionCount, status,option1) {
   {
     // alert(' in the for loop');
     if (globalVariable.option1[i].Option1.length == 0) {
-      create(i, 3);
+      createBefore(i, 3);
     }
     else if (globalVariable.option1[i].Option1.length == 1) {
       // this is for rating
     }
     else if (globalVariable.option1[i].Option1.length > 1) {
       // this is multiple choice
-      create(i, 1);
+      createBefore(i, 1);
 
     }
   }
