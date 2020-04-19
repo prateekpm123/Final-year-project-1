c2fa7c1e1a6bc8c37ea5b6f1ad6e2a9e2277a2d5
prateekpm123 <prateekpm123@gmail.com>
2020-04-20 00:20:50 +0530

    Created a basic logging system by creating a file Log.class.php and testing it on textarea-testing.php

---
+++ textarea-testing.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+
+require_once('Log.class.php');
+
+$Log = new Log();
+$Log->Write('test.txt', 'hey there whats is up my dude');
+
+$Log->Write('test.txt', 'The function is sucessfully working right now !!');
+
+
+?>
+
+
