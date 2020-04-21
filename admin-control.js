70ae4cc3b7d5b766ab7bf158b5e52ea1b0b083e2
prateekpm123 <prateekpm123@gmail.com>
2020-04-21 14:57:43 +0530

    Created a function to pass the formid to preview form.php and admin-control-form.php

---
+++ admin-control.js
@@ -0,0 +1,12 @@
+    // var globalVariable = {
+    //     x: 'sachin'
+    // };
+
+function passTheId(id) {
+    let val = id;
+    alert(id);
+    localStorage.setItem("myId", val);
+    window.location.href="admin-conttrol-form.php";
+    window.location.href = "preview-form.php";
+
+}
